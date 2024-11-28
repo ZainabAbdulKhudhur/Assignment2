5ad0b638c5fb43c825cf37aa394b74461f283ef3
ZainabAbdulKhudhur <zooza029zz@gmail.com>
2024-11-28 15:50:56 +0300

    ass2

--- index1.php
+++ index1.php
@@ -11,20 +11,25 @@ $data= json_decode($response, true);
 
 echo $response;
 print_r($response);
-if(!$data ||!isset($data[result])){
+
+if(!$data ||!isset($data["results"])){
     die('error fetching the data from the api');
 }
-$result=$data[results];
-print_r($result);
+$result=$data["results"];
+print_r( $result);
+
 ?>
 <html>
     <head>
-        <title></title>
+        <title>UOBstudent</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="sylesheet" herf="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+        <link rel="stylesheet" href="style.css">
 </head>
     <body>
+        <h1>University of Bahrain Students Enrollment by Nationality</h1>
         <table>
+        
             <thead>
                 <tr>
                 <th>Year</th>
@@ -37,15 +42,16 @@ print_r($result);
             </thead>
             <tbody>
                 <?php
-                foreach($result as $student){}
+                foreach($result as $student){
                     ?>
-                <td><?php echo $student["Year"]?></td>
-                <td><?php echo $student["Semster"]?></td>
-                <td><?php echo $student["ThePrograms"]?></td>
-                <td><?php echo $student["Nationality"]?></td>
-                <td><?php echo $student["Collages"]?></td>
-                <td><?php echo $student["Number of students"]?></td>
-                <?php 
+                <tr><td><?php echo $student["year"]?></td>
+                <td><?php echo $student["semester"]?></td>
+                <td><?php echo $student["the_programs"]?></td>
+                <td><?php echo $student["nationality"]?></td>
+                <td><?php echo $student["colleges"]?></td>
+                <td><?php echo $student["number_of_students"]?></td></tr>
+                
+                <?php }
                 ?>
             </tbody>
         </table>
